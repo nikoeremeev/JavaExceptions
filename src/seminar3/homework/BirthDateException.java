9457e575f309fa5f3fb0a8ed78997ba95de0e716
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-11-12 18:24:27 +0300

    Add Exception for BirthDate

---
+++ src/seminar3/homework/BirthDateException.java
@@ -0,0 +1,16 @@
+package seminar3.homework;
+
+public class BirthDateException extends Exception {
+
+    String data;
+
+    public BirthDateException(String inputData) {
+        this.data = inputData;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Ошибка ввода даты: '" + data + "'.\n" +
+                "Формат ввода даты рождения: 'дд.мм.гггг'.\n";
+    }
+}
